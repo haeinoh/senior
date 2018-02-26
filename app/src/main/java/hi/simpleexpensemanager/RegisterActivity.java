9fbaadd982ebf8c5492f2f7c72c0b939fcd49387
haeinoh <hoh3@mail.csuchico.edu>
2018-02-26 14:41:10 -0800

    Data (cont.)

---
+++ app/src/main/java/hi/simpleexpensemanager/RegisterActivity.java
@@ -0,0 +1,181 @@
+package hi.simpleexpensemanager;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private String userID;
+    private String userPassword;
+    private String userEmail;
+    private AlertDialog dialog;
+    private boolean validate = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final EditText emailText = (EditText) findViewById(R.id.emailText);
+
+        final Button validateButton = (Button) findViewById(R.id.validateButton);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = idText.getText().toString();
+                if(validate)
+                {
+                    return;
+                }
+                if(userID.equals(""))
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("ID should not be empty")
+                            .setPositiveButton("OK", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success)
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("You can use this ID")
+                                        .setPositiveButton("OK", null)
+                                        .create();
+                                dialog.show();
+                                idText.setEnabled(false);
+                                validate = true;
+                                idText.setBackgroundColor(getResources().getColor(R.color.colorWarning));
+                                validateButton.setBackgroundColor(getResources().getColor(R.color.colorWarning));
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("You cannot use this ID")
+                                        .setNegativeButton("OK", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userID, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+                String userEmail = emailText.getText().toString();
+
+                if(!validate)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("Please check your ID first")
+                            .setNegativeButton("OK", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+
+                if(userID.equals("") || userPassword.equals("") || userEmail.equals(""))
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("Blank is not allowed")
+                            .setNegativeButton("OK", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success)
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("Signup Failed")
+                                        .setPositiveButton("OK", null)
+                                        .create();
+                                dialog.show();
+                                finish();
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("You cannot use this ID")
+                                        .setNegativeButton("OK", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
+
+       /*TextView registerButton = (TextView) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent daymenuIntent = new Intent(RegisterActivity.this, DayActivity.class);
+                RegisterActivity.this.startActivity(daymenuIntent);
+            }
+        });*/
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(dialog != null)
+        {
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
+}
---
+++ app/src/main/java/hi/simpleexpensemanager/RegisterRequest.java
@@ -0,0 +1,30 @@
+package hi.simpleexpensemanager;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Haein on 2/26/2018.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    final static private String URL = "http://greenohi.cafe24.com/UserRegister.php";
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String userID, String userPassword, String userEmail, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID); //each paramter will match
+        parameters.put("userPassword", userPassword);
+        parameters.put("userEmail", userEmail);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
