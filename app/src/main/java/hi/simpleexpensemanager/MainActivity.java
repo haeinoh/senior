8894a96e1f3fcf32ea5c89b493449746cca048e5
haeinoh <hoh3@mail.csuchico.edu>
2018-02-28 00:43:47 -0800

    Main

--- app/src/main/java/hi/simpleexpensemanager/MainActivity.java
+++ app/src/main/java/hi/simpleexpensemanager/MainActivity.java
@@ -1,7 +1,12 @@
 package hi.simpleexpensemanager;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button todayButton = (Button) findViewById(R.id.todayButton);
+        final Button weekButton = (Button) findViewById(R.id.weekButton);
+        final Button monthButton = (Button) findViewById(R.id.monthButton);
+        final LinearLayout transaction = (LinearLayout) findViewById(R.id.transaction);
+
+        todayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction.setVisibility(View.GONE);
+                todayButton.setBackgroundColor(getResources().getColor(R.color.colorMenuDark));
+                weekButton.setBackgroundColor(getResources().getColor(R.color.colorMenuFragment));
+                monthButton.setBackgroundColor(getResources().getColor(R.color.colorMenuFragment));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new TodayFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        weekButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction.setVisibility(View.GONE);
+                todayButton.setBackgroundColor(getResources().getColor(R.color.colorMenuFragment));
+                weekButton.setBackgroundColor(getResources().getColor(R.color.colorMenuDark));
+                monthButton.setBackgroundColor(getResources().getColor(R.color.colorMenuFragment));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new WeekFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        monthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction.setVisibility(View.GONE);
+                todayButton.setBackgroundColor(getResources().getColor(R.color.colorMenuFragment));
+                weekButton.setBackgroundColor(getResources().getColor(R.color.colorMenuFragment));
+                monthButton.setBackgroundColor(getResources().getColor(R.color.colorMenuDark));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new MonthFragment());
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
