93cbfd6cc1c3922f496e87f21d751fa15eaccbb1
haeinoh <hoh3@mail.csuchico.edu>
2018-03-06 23:18:24 -0800

    Date done

--- app/src/main/java/hi/simpleexpensemanager/TodayFragment.java
+++ app/src/main/java/hi/simpleexpensemanager/TodayFragment.java
@@ -5,11 +5,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 /**
@@ -30,6 +35,25 @@ public class TodayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_today, container, false);
+
+        TextView yearLabel = (TextView)v.findViewById(R.id.yearLabel);
+        TextView monthLabel = (TextView)v.findViewById(R.id.monthLabel);
+        TextView dayLabel = (TextView)v.findViewById(R.id.dayLabel);
+        TextView weekLabel = (TextView)v.findViewById(R.id.weekLabel);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MMMM/d/E", Locale.US);
+        String strDate = sdf.format(cal.getTime());
+
+        String[] values = strDate.split("/",0);
+        /*for(int i = 0; i < values.length; i++)
+        {
+            Log.v("CHECK_DATE", values[i]);
+        }*/
+        yearLabel.setText(values[0]);
+        monthLabel.setText(values[1]);
+        dayLabel.setText(values[2]);
+        weekLabel.setText(values[3]);
 
         Button addIncomeButton = (Button) v.findViewById(R.id.addIncomeButton);
         addIncomeButton.setOnClickListener(new View.OnClickListener() {
@@ -52,4 +76,5 @@ public class TodayFragment extends Fragment {
         });
         return v;
     }
+
 }
