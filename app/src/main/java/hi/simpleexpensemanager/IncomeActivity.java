8158678c90512b972cf04e45d3476ebfddf33ff0
haeinoh <hoh3@mail.csuchico.edu>
2018-03-06 20:42:52 -0800

    finish add income, expense menu

---
+++ app/src/main/java/hi/simpleexpensemanager/IncomeActivity.java
@@ -0,0 +1,66 @@
+package hi.simpleexpensemanager;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
+
+import java.util.Calendar;
+
+public class IncomeActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+    private static final String TAG = "IncomeActivity";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income);
+        mDisplayDate = (TextView) findViewById(R.id.selectDate);
+
+        spinner = (Spinner) findViewById(R.id.incomeSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.income, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        IncomeActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+    }
+}
