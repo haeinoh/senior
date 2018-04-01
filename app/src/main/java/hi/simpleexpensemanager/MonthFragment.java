e866f4515b9d6fe759e7c6f574ab6254e32186ed
haeinoh <hoh3@mail.csuchico.edu>
2018-03-31 19:45:16 -0700

    Implement pie chart (default)

--- app/src/main/java/hi/simpleexpensemanager/MonthFragment.java
+++ app/src/main/java/hi/simpleexpensemanager/MonthFragment.java
@@ -1,12 +1,28 @@
 package hi.simpleexpensemanager;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Cache;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.lang.reflect.Array;
+import java.security.KeyStore;
+import java.util.ArrayList;
 
 
 /**
@@ -22,11 +38,54 @@ public class MonthFragment extends Fragment {
     public MonthFragment() {
         // Required empty public constructor
     }
+    PieChart pieChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_month, container, false);
+        View v = inflater.inflate(R.layout.fragment_month, container, false);
+
+        pieChart = (PieChart) v.findViewById(R.id.piechart);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.TRANSPARENT);
+        pieChart.setHoleRadius(30f);
+        //pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yvalues = new ArrayList<PieEntry>();
+
+            yvalues.add(new PieEntry(8f, "Category 1"));
+            yvalues.add(new PieEntry(15f, "Category 2"));
+            yvalues.add(new PieEntry(12f, "Category 3"));
+            yvalues.add(new PieEntry(25f, "Category 4"));
+            yvalues.add(new PieEntry(23f, "Category 5"));
+            yvalues.add(new PieEntry(17f, "Category 6"));
+
+        PieDataSet dataSet = new PieDataSet(yvalues,"Categories");
+
+        Description description = new Description();
+        description.setText("Month");
+        description.setTextSize(15);
+        pieChart.setDescription(description);
+
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(10f);
+
+        pieChart.setData(data);
+        data.setValueFormatter(new PercentFormatter());
+
+        return v;
     }
 }
