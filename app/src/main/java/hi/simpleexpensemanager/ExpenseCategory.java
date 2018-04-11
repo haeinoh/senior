d342a15117a72765725ed87173dc02f179a41157
haeinoh <hoh3@mail.csuchico.edu>
2018-04-10 22:27:06 -0700

    ArrayList->ListView(Setting-Category)

--- app/src/main/java/hi/simpleexpensemanager/ExpenseCategory.java
+++ app/src/main/java/hi/simpleexpensemanager/ExpenseCategory.java
@@ -2,6 +2,8 @@ package hi.simpleexpensemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ExpenseCategory extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class ExpenseCategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_category);
+
+
+        //From arrays.xml, get called "income" and store into String[] array
+        String[] expense = getResources().getStringArray(R.array.expense);
+
+        //Set into ArrayAdapter
+        //simple_list_item_1 is stored textview (xml)
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, expense);
+
+        //From resource, get Listview and store it into adapter
+        ListView expenseCategoryView = (ListView) findViewById(R.id.expenseCategoryView);
+        expenseCategoryView.setAdapter(adapter);
     }
 }
--- app/src/main/java/hi/simpleexpensemanager/IncomeCategory.java
+++ app/src/main/java/hi/simpleexpensemanager/IncomeCategory.java
@@ -3,17 +3,25 @@ package hi.simpleexpensemanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class IncomeCategory extends AppCompatActivity {
-
-    private ArrayAdapter adapter;
-    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income_category);
 
+        //From arrays.xml, get called "income" and store into String[] array
+        String[] income = getResources().getStringArray(R.array.income);
+
+        //Set into ArrayAdapter
+        //simple_list_item_1 is stored textview (xml)
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, income);
+
+        //From resource, get Listview and store it into adapter
+        ListView incomeCategoryView = (ListView) findViewById(R.id.incomeCategoryView);
+        incomeCategoryView.setAdapter(adapter);
     }
 }
